dd2a321affbb6e0cb5da0dbb7b4693587619d378
Rafael Almeida <ralms@ralms.net>
2020-03-23 13:05:44 -0300

    Implementation Service for Information cases Covid19

---
+++ backend/AjudaSolidaria/src/AjudaSolidaria.Core/Services/Covid19/ICidadeService.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AjudaSolidaria.Core.Services.Covid19
+{
+    public interface ICovid19Service
+    {
+        ValueTask<IEnumerable<Domain.Data.Covid19Total>> GetCasesTotalAsync();
+        ValueTask<IEnumerable<Domain.Data.Covid19Estado>> GetCasesStatesAsync();
+        ValueTask<IEnumerable<Domain.Data.Covid19Cidade>> GetCasesCitiesByStateAsync(string state);
+        ValueTask<IEnumerable<Domain.Data.Covid19Cidade>> GetCasesCitiesAsync();
+    }
+}
---
+++ backend/AjudaSolidaria/src/AjudaSolidaria.Domain/Data/Covid19Total.cs
@@ -0,0 +1,25 @@
+﻿using CsvHelper.Configuration.Attributes;
+
+namespace AjudaSolidaria.Domain.Data
+{
+    public class Covid19Total
+    {
+        [Index(0)]
+        public string Pais { get; set; }
+
+        [Index(1)]
+        public string Estado { get; set; }
+
+        [Index(2)]
+        public string Total { get; set; }
+
+        [Index(3)]
+        public string TotalConfirmados { get; set; }
+
+        [Index(4)]
+        public string TotalNaoConfirmados { get; set; }
+
+        [Index(5)]
+        public string TotalMortes { get; set; }
+    }
+}
