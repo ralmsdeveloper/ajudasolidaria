6c49dce520ddafd1b54d538633f53df19e46395c
Daniel Waldschmidt de Alencar <chawgid@msn.com>
2020-03-24 21:06:05 -0300

    Last changes (#5)
    * frontend first version
    
    * changes

--- frontend/Startup.cs
+++ frontend/Startup.cs
@@ -1,3 +1,5 @@
+using AjudaSolidaria.Shared;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Builder;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -7,6 +9,9 @@ namespace AjudaSolidaria
     {
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAuthorizationCore();
+            services.AddScoped<AuthenticationStateProviders.TokenAuthenticationStateProvider>();
+            services.AddScoped<AuthenticationStateProvider>(provider => provider.GetRequiredService<AuthenticationStateProviders.TokenAuthenticationStateProvider>());
         }
 
         public void Configure(IComponentsApplicationBuilder app)
